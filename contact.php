cf3027d5c872ba80bca332e72ced64a0aec54bfe
Mohamed Mbengue <slasher1>
2015-12-19 10:47:48 -0500

    push

--- contact.php
+++ contact.php
@@ -5,7 +5,9 @@ $site_data[PAGE_ID] = "Contact";
 require_once "view_parts/_page_base.php"; // référence au fichier de référence page_base = HEAD en HTML
 ?>
 
-<div id="main"></div>
+<div id="main">
+
+</div>
 
 <?php
 require_once "view_parts/_page_bottom.php"; // référence au fichier de référence page_base = HEAD en HTML
