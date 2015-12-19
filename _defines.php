5376ae9c40b7b92f2684ba5c363a53f0868987a2
Mohamed Mbengue <slasher1>
2015-12-19 18:04:01 -0500

    base de donnes

--- _defines.php
+++ _defines.php
@@ -3,3 +3,21 @@ define("SITE_NAME" , "PHPat");
 define("SITE_URL", "http://phpat.projet.isi.com");
 define("PAGE_ID", "page_id");
 define("USER_IS_LOGGED", "user_is_logged"); // L'utilisateur est connecté ou non
+
+// Declaration des des variables pour la creation a notre base de donnees
+
+$servername = "localhost";
+$username = "turntablelist";
+$password = "p62";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+mysqli_close($conn);
+
--- view_parts/_login_form.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pcantin
- * Date: 2015-12-17
- * Time: 16:35
- */
